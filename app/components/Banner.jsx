6bd2fc32843b5c0f9cad097df02b5125c61b8003
PaccyC <abayisengaaimepacifique@gmail.com>
2024-08-10 00:26:08 +0200

    added custom not found page

--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
-
+import Image from "next/image"
+import Link from "next/link"
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className='hero-banner-container'>
+      <div className=''>
+        <p className='beats-solo'>SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <Image 
+          //  src={}
+           width={450}
+           height={450}
+           alt="headphones"
+           className="hero-banner-image"
+           />
+
+           <div>
+            <Link href="/product/ID">
+            
+            <button type="button">BUTTON TEXT</button>
+            </Link>
+           </div>
+      </div>
+
+    </div>
   )
 }
 
---
+++ app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        Sorry, the page requested was not found
+    </div>
+  )
+}
+
+export default page
